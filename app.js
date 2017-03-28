48912f4a741971423e78ecd9b4996a89c4184047
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-03-28 09:55:42 -1000

    First version

---
+++ app.js
@@ -0,0 +1,8 @@
+$(function() {
+  $('#mainButton').hover(function() {
+    $("body").css('background-color', '#26E9C7');
+  }, function() {
+    $("body").css('background-color', '#0770BF');
+  });
+});
+
